d7037b77ae469c32de4d4b6b43631de65ad5f69a
SreekanthKandala <105445924+SreekanthKandala@users.noreply.github.com>
2022-05-12 23:56:48 +0530

    Create StringReplace.java

---
+++ StringReplace.java
@@ -0,0 +1,6 @@
+public class ReplaceString{  
+public static void main(String args[]){  
+String s1="Hi Skand Welcome to Unisys ";  
+String replaceString=s1.replace("Skand","Sreekanth");//replaces all occurrences of "Skand" to "Skand"  
+System.out.println(replaceString);  
+}}  
